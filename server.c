ac964d3b4c5ef0ec5b000765b4cfbcaaf8966413
Adin355 <adincaspary@gmail.com>
2024-05-13 21:26:03 -0400

    loop infinity

--- server.c
+++ server.c
@@ -77,51 +77,53 @@ int main(int argc, char*argv[]) {
   }
   
   // prepares server to accept connections
-  // 3 : server will queue 3 connections.
-  if (listen(server, 3) < 0) {
+  // 5 : server will queue 5 connections.
+  if (listen(server, 5) < 0) {
     printf("listen failed\n");
     close(server);
     return(1);
   }
 
-  // store client address
-  struct sockaddr_in client_address;
-  socklen_t client_address_len = sizeof(client_address);
+  while (true) {
+    // store client address
+    struct sockaddr_in client_address;
+    socklen_t client_address_len = sizeof(client_address);
   
-  // accepts connection on server
-  int newsock = accept(server, (struct sockaddr *)&client_address, &client_address_len);
-  char client_ip[INET_ADDRSTRLEN];
+    // accepts connection on server
+    int newsock = accept(server, (struct sockaddr *)&client_address, &client_address_len);
+    char client_ip[INET_ADDRSTRLEN];
   
-  inet_ntop(AF_INET, &client_address.sin_addr, client_ip, INET_ADDRSTRLEN);
-  printf("Connection accepted from %s:%d\n", client_ip, ntohs(client_address.sin_port));
-  char buffer[1024] = {0};
-  int read = recv(newsock, buffer, sizeof(buffer), 0);
+    inet_ntop(AF_INET, &client_address.sin_addr, client_ip, INET_ADDRSTRLEN);
+    printf("Connection accepted from %s:%d\n", client_ip, ntohs(client_address.sin_port));
+    char buffer[1024] = {0};
+    int read = recv(newsock, buffer, sizeof(buffer), 0);
   
-  if(read > 0) {
-    printf("The server recived: %s\n", buffer);
-    buffer[read] = '\0'; // ensure null termination
+    if(read > 0) {
+      printf("The server recived: %s\n", buffer);
+      buffer[read] = '\0'; // ensure null termination
     
-    // convert to lower case
-    for(int i = 0; buffer[i]; i++) {
-      buffer[i] = tolower(buffer[i]); 
-    }
+      // convert to lower case
+      for(int i = 0; buffer[i]; i++) {
+	buffer[i] = tolower(buffer[i]); 
+      }
 
-    if(strcmp(buffer, "on") == 0) {
-      set_pins(1, 1, 1, 1);
-      printf("motors == 1\n"); 
-    } else if (strcmp(buffer, "off") == 0) {
-      set_pins(0, 0, 0, 0);
-      printf("motors == 0\n");
-    } else {
-      printf("invalid command"); 
-    }
+      if(strcmp(buffer, "on") == 0) {
+	set_pins(1, 1, 1, 1);
+	printf("motors == 1\n"); 
+      } else if (strcmp(buffer, "off") == 0) {
+	set_pins(0, 0, 0, 0);
+	printf("motors == 0\n");
+      } else {
+	printf("invalid command"); 
+      }
     
-  } else if (read == 0) {
-    printf("connection closed by client\n");
-  } else {
-    printf("error reading\n");
+    } else if (read == 0) {
+      printf("connection closed by client\n");
+    } else {
+      printf("error reading\n");
+    }
+    close(newsock);
   }
-  close(newsock);
   close(server);
   return 0;
 }
